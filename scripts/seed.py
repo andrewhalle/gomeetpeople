6a45e5559f0fb706b6f9a0536a5a99d453a6bcd6
Anna Leskova <aleskova@berkeley.edu>
2017-11-07 13:18:27 -0800

    pop-up logic complete, but erroring out randomly

--- scripts/seed.py
+++ scripts/seed.py
@@ -17,6 +17,6 @@ if dev.is_file():
     
 # Create and seed the database
 db.create_all()
-db.session.add(User(username="andrew", latitude=37.871853, longitude=-122.258423, active=True, last_request=datetime.datetime.now()))
+db.session.add(User(username="andrew", latitude=37.875594, longitude=-122.258786, active=True, last_request=datetime.datetime.now()))
 db.session.add(User(username="chris", latitude=37.877207, longitude=-122.259485, active=True, last_request=datetime.datetime.now()))
 db.session.commit()
