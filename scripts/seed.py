0ccdb0a56a3afa6f121bf5ed0c521e47dde109ad
Anna Leskova <aleskova@berkeley.edu>
2017-11-07 16:59:59 -0800

    use my seed please

--- scripts/seed.py
+++ scripts/seed.py
@@ -17,6 +17,6 @@ if dev.is_file():
     
 # Create and seed the database
 db.create_all()
-db.session.add(User(username="andrew", rating="4 / 5", latitude=37.871853, longitude=-122.258423, active=True, last_request=datetime.datetime.now()))
+db.session.add(User(username="andrew", rating="4 / 5", latitude=37.875727, longitude=-122.257858, active=True, last_request=datetime.datetime.now()))
 db.session.add(User(username="chris", rating="4 / 5", latitude=37.877207, longitude=-122.259485, active=True, last_request=datetime.datetime.now()))
 db.session.commit()
